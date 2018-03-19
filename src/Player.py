d38d723148a1672c7a13160d0a943dc30d55db0d
Seiya Yamamoto <vim.yamagen@gmail.com>
2018-03-19 20:52:54 +0900

    delete python files

--- src/Player.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf8 -*-
-from PlayerObject import PlayerObject
-
-
-class Player(PlayerObject):
-
-    def __init__(self, myTurn):
-        super().__init__(myTurn)
